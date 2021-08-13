70f3c042a19d4a82a0239d7400342e20d781e3c4
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-13 23:26:23 +0300

    Create PerfectDivisors.cs

---
+++ PerfectDivisors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace If_else_And_loop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt16(textBox1.Text);
+            for(int i = 1; i <= num; i++)
+            {
+                if(num %i == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+    }
+}
